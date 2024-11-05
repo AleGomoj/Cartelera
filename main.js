cdb0a7d873b1db7f370654f79979003837758e8f
Alejandro Gómez Ojeda <97414326+AleGomoj@users.noreply.github.com>
2024-11-05 09:41:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const requestURL = "../json/documentaries.json";
+
+async function fetchMoviesJson() {
+    const response = await fetch(requestURL);
+    const movies = await response.json();
+
+    return movies;
+}
+
+fetchMoviesJson().then(movies => {
+    for (let i = 0; i < movies.documentaries.length; i++){
+        const moviesSection = document.getElementById
+        ("movieSection");
+
+        let id = movies.documentaries[i].id;
+        let poster = movies.documentaries[i].poster;
+        let title = movies.documentaries[i].title;
+        let year = movies.documentaries[i].year;
+        let length = movies.documentaries[i].length;
+        let director = movies.documentaries[i].director;
+        let synopsis = movies.documentaries[i].synopsis;
+
+        moviesSection.innerHTML += `<div class="card" style="width: 18rem;">
+        <img src="${poster}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>`
+    }
+})
